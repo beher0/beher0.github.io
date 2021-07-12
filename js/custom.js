c0b01b541f4c4570e439b4cecc44e5d22dd386f9
alovelyyou <noti163@163.com>
2021-07-12 23:14:53 +0800

    Site updated: 2021-07-12 23:14:52

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,1 @@
-// tabBar jS 样式 点击复制内容
-function myFunction(message) {
-
-    var input = document.createElement("input");
-    input.value = message;
-    document.body.appendChild(input);
-    input.select();
-    input.setSelectionRange(0, input.value.length), document.execCommand('Copy');
-    document.body.removeChild(input);
-    alert("复制成功", "text");
 }
