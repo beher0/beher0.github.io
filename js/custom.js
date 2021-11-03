d8dc497f1b5909153be4f48cd6c41d405db28af6
alovelyyou <noti163@163.com>
2021-11-03 20:44:31 +0800

    Site updated: 2021-11-03 20:44:28

--- js/custom.js
+++ js/custom.js
@@ -52,3 +52,26 @@
     </div>`
 
   document.body.appendChild(slDiv)
+
+
+/**
+ * 获取 文章标题 H1 H2 H3
+ */
+var simpleH1 = document.querySelectorAll('.post-content>h1');
+var simpleH2 = document.querySelectorAll('.post-content>h2');
+var simpleH3 = document.querySelectorAll('.post-content>h3');
+simpleH1.forEach((ele)=>{
+  var span = document.createElement('span')
+  span.innerHTML = '<cst class="red">H1 </cst>'
+  ele.insertBefore(span,ele.childNodes[0])
+})
+simpleH2.forEach((ele)=>{
+  var span = document.createElement('span')
+  span.innerHTML = '<cst>H2 </cst>'
+  ele.insertBefore(span,ele.childNodes[0])
+})
+simpleH3.forEach((ele)=>{
+  var span = document.createElement('span')
+  span.innerHTML = '<cst class="pink">H3 </cst>'
+  ele.insertBefore(span,ele.childNodes[0])
+})
